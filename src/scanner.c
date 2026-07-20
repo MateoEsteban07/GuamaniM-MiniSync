7a285c308dd0b54cc30f4183679f75a1fee83ed9
Mateo Esteban Guamaní Barahona <mateoestebang07@gmail.com>
2026-07-19 20:22:42 -0500

    Fix syntax error in scanner.c by closing dir

--- src/scanner.c
+++ src/scanner.c
@@ -133,8 +133,8 @@ void sincronizar_directorios(const char *origen, const char *destino, int pipe_e
                 }
             }
     }
-    closedir(dir);
+} closedir(dir);
 
     limpiar_archivos_eliminados(origen, destino);
-    }
+    
 }
